6df281d56ed3d406c3b1f1e6aa93ef8d5e385ebb
takatoka <48255038+takatoka@users.noreply.github.com>
2019-03-10 22:48:25 +0900

    Add: add DesignPattern solution and FactoryMethod Project
    factorymethodプロジェクトを追加しました。

---
+++ DesignPattern.FactoryMethod/GamePlayerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.FactoryMethod
+{
+    abstract class GamePlayerFactory
+    {
+        public GamePlayer Create(string[] titles)
+        {
+            return CreatePlayer(titles);
+        }
+
+        protected abstract GamePlayer CreatePlayer(string[] titles);
+    }
+}
---
+++ DesignPattern.FactoryMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DesignPattern.FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] swSofts = new[] { "Mario", "Zelda", "Pokemon", "Smash Bros." };
+
+            GamePlayerFactory swFactory = new SwitchPlayerFactory();
+            swFactory.Create(swSofts);
+
+            Console.ReadLine();
+        }
+    }
+}
