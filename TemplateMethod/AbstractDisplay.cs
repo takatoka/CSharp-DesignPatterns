0325b44050ff616c4422524368f92ee6374fe968
takatoka <48255038+takatoka@users.noreply.github.com>
2019-03-21 16:31:22 +0900

    Add: Add template method project

---
+++ TemplateMethod/AbstractDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod
+{
+    /// <summary>
+    /// Abstract クラス
+    /// 処理の枠組み、処理の流れ、アルゴリズムを記述しておく
+    /// 処理の具体的な内容は実装しない
+    /// 順番だけ！！！！
+    /// </summary>
+    public abstract class AbstractDisplay
+    {
+        public abstract void Open();
+        public abstract void Print();
+        public abstract void Close();
+        public void Display()
+        {
+            Open();
+            for (int i = 0; i < 5; i++)
+                Print();
+            Close();
+        }
+    }
+}
---
+++ TemplateMethod/CharDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod
+{
+    /// <summary>
+    /// 具体的な内容を記述するクラス
+    /// 処理の順番は意識しない
+    /// </summary>
+    public class CharDisplay : AbstractDisplay
+    {
+        private Char _chr;
+
+        public CharDisplay(Char chr)
+        {
+            _chr = chr;
+        }
+
+        public override void Open()
+        {
+            Console.Write("<<");
+        }
+
+        public override void Print()
+        {
+            Console.Write(_chr);
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine(">>");
+        }
+    }
+}
