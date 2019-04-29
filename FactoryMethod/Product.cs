c78137b3aa57521a812792356ebd3f7dcc47bceb
takatoka <48255038+takatoka@users.noreply.github.com>
2019-04-30 04:25:21 +0900

    Add add factory method pattern project
    ファクトリ＾メソッドパターンを実装しました。

---
+++ FactoryMethod/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    /// <summary>
+    /// 製品を表す抽象クラス
+    /// Factoryで生成したいクラスの親クラスになる
+    /// </summary>
+    public abstract class Product
+    {
+        public abstract void Use();
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new IDCardFactory();
+            Product card1 = factory.Create("Taro Tanaka");
+            Product card2 = factory.Create("Hanako Yamada");
+            Product card3 = factory.Create("Kenji Sato");
+            card1.Use();
+            card2.Use();
+            card3.Use();
+
+            Console.ReadLine();
+        }
+    }
+}
